039261d5382c910e620dd1edd2704f325dd07cbe
chrisrold senora <youexample.com>
2018-12-14 01:10:33 -0600

    changes

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_2f267b7ef0e048a19e204689fdca6b05,
+  secret: process.env.SPOTIFY_b0ce4d0512814038b0ab7a53d5080482,
+};
+
+
